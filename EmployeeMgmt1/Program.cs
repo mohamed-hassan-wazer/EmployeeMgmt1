0ca35a4d908f270c93134059a48a53ec2af82dff
mohamedhassan161616@gmail.com <mohamedhassan161616@gmail.com>
2023-01-25 14:26:42 +0200

    change the file

--- EmployeeMgmt1/Program.cs
+++ EmployeeMgmt1/Program.cs
@@ -16,7 +16,7 @@ namespace EmployeeMgmt1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new login());
+            Application.Run(new Employees ());
         }
     }
 }
