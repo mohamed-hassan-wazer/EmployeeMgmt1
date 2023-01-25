2f8d25a015d0c8b2dd2a52a0ec7d68701cad29df
mohamedhassan161616@gmail.com <mohamedhassan161616@gmail.com>
2023-01-25 14:50:44 +0200

    change color

--- EmployeeMgmt1/Employees.Designer.cs
+++ EmployeeMgmt1/Employees.Designer.cs
@@ -228,7 +228,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Teal;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.Teal;
+            this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Location = new System.Drawing.Point(132, 547);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(117, 35);
