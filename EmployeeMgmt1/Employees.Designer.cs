b2a72cbf93af924c924175a3f06dbea8cd832c95
mohamedhassan161616@gmail.com <mohamedhassan161616@gmail.com>
2023-01-25 14:13:00 +0200

    change text color

--- EmployeeMgmt1/Employees.Designer.cs
+++ EmployeeMgmt1/Employees.Designer.cs
@@ -46,6 +46,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(642, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(369, 25);
