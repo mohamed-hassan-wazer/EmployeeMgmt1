11500bedbf1730b3785e68d7a2ed02023cc1f7b3
mohamedhassan161616@gmail.com <mohamedhassan161616@gmail.com>
2023-01-25 14:59:37 +0200

    change color

--- EmployeeMgmt1/Department.Designer.cs
+++ EmployeeMgmt1/Department.Designer.cs
@@ -34,6 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1200, 703);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
