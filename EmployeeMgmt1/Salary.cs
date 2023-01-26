1cc8047b9115bf6efe506eced4ef876a7633cfdb
mohamedhassan161616@gmail.com <mohamedhassan161616@gmail.com>
2023-01-26 19:13:47 +0200

    remove condition

--- EmployeeMgmt1/Salary.cs
+++ EmployeeMgmt1/Salary.cs
@@ -45,10 +45,6 @@ namespace EmployeeMgmt1
             {
                 AmountTb.Text = "" +(d * DSal);
             }
-            else if(Convert.ToInt32(DaysTb.Text) > 31)
-            {
-                MessageBox.Show("Days can not be Greater than 31")
-            }
             else
             {
                 d = Convert.ToInt32(DaysTb.Text);
