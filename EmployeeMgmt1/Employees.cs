ba6d88a5e1c757d23e2dfeddaf5d1f65cee69328
mohamedhassan161616@gmail.com <mohamedhassan161616@gmail.com>
2023-01-26 16:51:00 +0200

    make changes

--- EmployeeMgmt1/Employees.cs
+++ EmployeeMgmt1/Employees.cs
@@ -104,7 +104,35 @@ namespace EmployeeMgmt1
 
         private void DeleteBtn_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                if(key == 0)               
+                {
+                    MessageBox.Show("missing data!!!");
+                }
+                else
+                {
+                    string Name = EmpNameTb.Text;
+                    string Gender = GenCb.SelectedItem.ToString();
+                    int Dep = Convert.ToInt32(DepCb.SelectedValue.ToString());
+                    string DOB = DOBTb.Value.ToString();
+                    string JDate = JDateTb.Value.ToString();
+                    int Salary = Convert.ToInt32(DailySalTb.Text);
+                    string Query = "Delete from EmployeeTb1 where Empid= {0}";
+                    Query = string.Format(Query, Name, Gender, Dep, DOB, JDate, Salary, key);
+                    Con.SetData(Query);
+                    ShowEmp();
+                    MessageBox.Show("Emoloyee Updated!!!");
+                    EmpNameTb.Text = "";
+                    DailySalTb.Text = "";
+                    GenCb.SelectedIndex = -1;
+                    DepCb.SelectedIndex = -1;
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
         }
 
         private void UpdateBtn_Click(object sender, EventArgs e)
