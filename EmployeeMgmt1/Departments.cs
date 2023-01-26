69ab1ed3c288a49c460ce2fb8dd0c44e7bc6acf9
mohamedhassan161616@gmail.com <mohamedhassan161616@gmail.com>
2023-01-26 13:51:05 +0200

    add listerDepartments()

--- EmployeeMgmt1/Departments.cs
+++ EmployeeMgmt1/Departments.cs
@@ -17,6 +17,7 @@ namespace EmployeeMgmt1
         {
             InitializeComponent();
             Con = new Functions();
+            listerDepartments();
         }
         private void listerDepartments() 
         {
